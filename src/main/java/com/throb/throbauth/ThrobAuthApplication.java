923687b5492bdc3a21d63656237da35c7e7dfc36
safeint <chaim.chu92@gmail.com>
2024-05-09 16:37:23 +0800

    init

---
+++ src/main/java/com/throb/throbauth/ThrobAuthApplication.java
@@ -0,0 +1,13 @@
+package com.throb.throbauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThrobAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThrobAuthApplication.class, args);
+    }
+
+}
